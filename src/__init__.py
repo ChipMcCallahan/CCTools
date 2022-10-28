797845e3f9ea4e3986b314c70a223d72ab822f87
ChipMcCallahan <thisischipmccallahan@gmail.com>
2022-10-27 22:26:09 -0700

    small progress on CC1Level class

---
+++ src/__init__.py
@@ -0,0 +1,2 @@
+"""Module for working with Chip's Challenge 1 levels and levelsets."""
+from .cc1_levels import *
---
+++ src/cc1_levels.py
@@ -0,0 +1,14 @@
+"""Contains the CC1Level class."""
+
+
+class CC1Level:
+    """A class to represent a Chips Challenge 1 Level"""
+    def __init__(self):
+        """init"""
+
+    @staticmethod
+    def parse_dat(dat):
+        """Parse from DAT file."""
+
+    def to_dat(self):
+        """Write to DAT file."""
