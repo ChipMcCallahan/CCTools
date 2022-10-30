9fafbfb6b4c641b8c83a2d80a11b3f32068d89b0
ChipMcCallahan <thisischipmccallahan@gmail.com>
2022-10-29 19:48:16 -0700

    Add cc1_tile_code.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,5 @@
 """Module for working with Chip's Challenge 1 levels and levelsets."""
 from .cc1_level import *
 from .cc1_levelset import *
+from .cc1_tile_code import *
 from .dat_retriever import *
